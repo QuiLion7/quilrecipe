af2e869c7818191221332de8556357dcf1babd8f
QuiLion7 <quilbrub@gmail.com>
2024-06-25 00:54:54 -0300

    feat: create MobileSidebar component

---
+++ src/components/MobileSidebar.tsx
@@ -0,0 +1,17 @@
+import { Heart, Home } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const MobileSidebar = () => {
+  return (
+    <div className="flex justify-center gap-10 border-t fixed w-full bottom-0 left-0 bg-white x-10 p-2 sm:hidden">
+      <Link to={"/"}>
+        <Home size={24} className="cursor-pointer" />
+      </Link>
+      <Link to={"/favorites"}>
+        <Heart size={24} className="cursor-pointer" />
+      </Link>
+    </div>
+  );
+};
+
+export default MobileSidebar;
