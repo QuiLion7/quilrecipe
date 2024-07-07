2ee521a03e41a188615775ae23136efad6e9f0f8
QuiLion7 <quilbrub@gmail.com>
2024-07-07 18:40:10 -0300

    feat: Layout adjustments, improved responsiveness, and added transition effects

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -1,14 +1,29 @@
 import { Heart, Home } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const MobileSidebar = () => {
+  const location = useLocation();
   return (
-    <div className="flex justify-center gap-10 border-t fixed w-full bottom-0 left-0 bg-white x-10 p-2 sm:hidden">
+    <div className="flex justify-center gap-10 border-t fixed w-full bottom-0 left-0 bg-white x-10 p-2 sm:hidden z-30">
       <Link to={"/"}>
-        <Home size={24} className="cursor-pointer" />
+        <Home
+          size={40}
+          className={`cursor-pointer p-2 rounded-full hover:bg-[#fd9d27] transition-all duration-300] ${
+            location.pathname === "/"
+              ? "bg-[#1fa5c0] text-white font-extrabold"
+              : ""
+          }`}
+        />
       </Link>
       <Link to={"/favorites"}>
-        <Heart size={24} className="cursor-pointer" />
+        <Heart
+          size={40}
+          className={`cursor-pointer p-2 rounded-full hover:bg-[#fd9d27] transition-all duration-300] ${
+            location.pathname === "/favorites"
+              ? "bg-[#1fa5c0] text-white font-extrabold"
+              : ""
+          }`}
+        />
       </Link>
     </div>
   );
